0d8dd61c17630f196d3ed476b131b79f93a94146
Dario Chamorro <dario.chamorro.v@gmail.com>
2018-12-12 21:03:03 -0500

    DataBinding implemented

--- app/src/main/java/com/example/darfe/constraintanim/MainActivity.kt
+++ app/src/main/java/com/example/darfe/constraintanim/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.darfe.constraintanim
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.constraint.ConstraintSet
 import android.support.transition.TransitionManager
+import com.example.darfe.constraintanim.databinding.ActivityMainExpandedBinding
+import com.example.darfe.constraintanim.models.Movie
 import kotlinx.android.synthetic.main.activity_main_expanded.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,17 +18,21 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_expanded)
+        val binding:ActivityMainExpandedBinding = DataBindingUtil
+            .setContentView(this, R.layout.activity_main_expanded)
+
+
 
         setExpanded.clone(root)
         setCollapsed.clone(this, R.layout.activity_main)
 
-        time.text = "2 horas"
-        today.text = "12 de Diciembre"
+        val movie = Movie("Acuaman",
+            "https://img.elcomercio.pe/files/article_content_ec_fotos/uploads/2018/12/11/5c1017702c38e.jpeg",
+            "12 de Diciembre",
+            "2 horas",
+            "La mejor Pelicula")
 
-        des.text = "La mejor pelicula ..."
-        name.text = "Acuaman"
-        img.setImageURI("https://img.elcomercio.pe/files/article_content_ec_fotos/uploads/2018/12/11/5c1017702c38e.jpeg")
+        binding.movie = movie
 
         btnInfo.setOnClickListener{
             TransitionManager.beginDelayedTransition(root)
